eb5a1a00a3cbdba546951aa886488f84e4c5c2ab
Md Zahid Hasan <arnabrahmanzahid03@gmail.com>
2025-07-28 14:33:37 +0600

    java-apache-tomcat

---
+++ java-apache-tomcat/src/main/java/com/example/java_apache_tomcat/JavaApacheTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_apache_tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApacheTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApacheTomcatApplication.class, args);
+	}
+
+}
---
+++ java-apache-tomcat/src/main/java/com/example/java_apache_tomcat/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.java_apache_tomcat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaApacheTomcatApplication.class);
+	}
+
+}
