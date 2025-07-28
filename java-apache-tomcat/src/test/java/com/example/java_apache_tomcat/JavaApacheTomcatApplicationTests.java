6c33dc2b417cd6469276c4f0277c2889f4c9cc7b
Md Zahid Hasan <zahid.s.dev@gmail.com>
2025-07-28 14:33:37 +0600

    java-apache-tomcat

---
+++ java-apache-tomcat/src/test/java/com/example/java_apache_tomcat/JavaApacheTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_apache_tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApacheTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
