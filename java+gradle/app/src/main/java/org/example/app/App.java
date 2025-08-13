9800ca29b8f9ae690bc10138af6889b44fd15c05
Md Zahid Hasan <zahid.s.dev@gmail.com>
2025-08-13 13:56:04 +0600

    java+gradle

---
+++ java+gradle/app/src/main/java/org/example/app/App.java
@@ -0,0 +1,26 @@
+package org.example.app;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.start();
+        System.out.println("Server started at http://localhost:8080/");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws java.io.IOException {
+            String response = "Hello from Java app!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
